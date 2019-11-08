d762ab17c1dccf25251cad2f20143db8876a2c91
Edwin <edwin_engelbrethsen@outlook.com>
2019-11-09 00:11:27 +0100

    added title to frontpage

--- Pages/Home.page.js
+++ Pages/Home.page.js
@@ -3,7 +3,7 @@ const HomePage = {
                 <div id="homePage">
                 
                     <img id="TestImage" src="./Bilder/HK_logo_icon.png">
-                    
+                    <h3 id="TestTitle">Studentklinikken</h3>
                 </div>
             `,
     data() {
