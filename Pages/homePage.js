14e95bc2c3bfee5d85cfc585520862fd1358039b
TobiasLab <tobias.labidi@gmail.com>
2019-11-27 00:54:34 +0100

    hardcoded temporary password to log in to the page

--- Pages/homePage.js
+++ Pages/homePage.js
@@ -1,3 +1,15 @@
 setTimeout(function() {
     $('.hide').show();
 }, 3000);
+
+
+function authenticateLogin() {
+    let pass = document.getElementById("password").value;
+    let storedPW = "123"; //Temp PW for the page
+
+    if (pass === storedPW) {
+        document.getElementById("log_in_button").setAttribute("onclick", "location.href='/HTML/landingPage.html'");
+    } else {
+        alert("You typed in the wrong password");
+    }
+};
